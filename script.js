98e77cdb2bc583e65f6c335392a440983be6df79
SolPulido <144694649+SolPulido@users.noreply.github.com>
2024-10-30 10:03:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// You can add interactivity here if needed
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Thank you for your message!');
