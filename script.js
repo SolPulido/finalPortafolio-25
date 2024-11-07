81a833ef289ada325897c02737a4b5f338f940bd
SolPulido <144694649+SolPulido@users.noreply.github.com>
2024-11-07 11:06:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const background = document.getElementById('background');
-
-document.addEventListener('mousemove', (e) => {
-    const x = e.clientX / window.innerWidth;
-    const y = e.clientY / window.innerHeight;
-    background.style.background = `linear-gradient(${x * 360}deg, #26495e, #91abbd)`;
-});
 
